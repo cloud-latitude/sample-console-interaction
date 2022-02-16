e56c3d46ed8f94d2e874c7577aab2c15cb800050
Yu-Song Syu <kuma@cloud-latitude.com>
2022-02-16 20:30:39 +0800

    add test

---
+++ src/main/java/org/example/GetWelcomeMessageService.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class GetWelcomeMessageService {
+
+    public String getWelcomeMessage() {
+        return "";
+    }
+
+}
---
+++ src/test/java/org/example/GetWelcomeMessageServiceTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class GetWelcomeMessageServiceTest {
+
+    @Test
+    void name() {
+        Assertions.assertEquals(3, 3);
+    }
+}
